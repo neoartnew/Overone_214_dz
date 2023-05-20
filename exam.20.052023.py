a875c05a55ea69e5c28390fa589ec73ba3a2b76e
Art <134142716+neoartnew@users.noreply.github.com>
2023-05-20 22:23:22 +0300

    Update exam.20.052023.py

--- exam.20.052023.py
+++ exam.20.052023.py
@@ -6,21 +6,21 @@
 # звездочкам
 
 
-# def cover_card_number(card_number):
-#     covered_number = '*' * (len(card_number) - 4) + card_number[-4:]
-#     return covered_number
-# card_number = '5864286345695226'
-# covered_card_number = cover_card_number(card_number)
-# print(covered_card_number)  # Выведет: ************3456
+def cover_card_number(card_number):
+     covered_number = '*' * (len(card_number) - 4) + card_number[-4:]
+     return covered_number
+ card_number = '5864286345695226'
+ covered_card_number = cover_card_number(card_number)
+ print(covered_card_number)  # Выведет: ************3456
 
 
 # Задача № 2:
 #
 # Напишите функцию, которая проверяет: является ли слово палиндромом
 
-# def is_palindrome(word):
-#     return word == word[::-1]
-#
-# print(is_palindrome("deed")) # True
-# print(is_palindrome("hello")) # False
-#
+ def is_palindrome(word):
+     return word == word[::-1]
+
+ print(is_palindrome("deed")) 
+ print(is_palindrome("hello")) 
+
